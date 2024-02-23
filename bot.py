7985361f3d0319cad0450ca32c6dc3d0aa9a7c57
alinkalina <alinkameshch@yandex.ru>
2024-02-23 11:40:47 +0300

    реализация продолжения ответа
    Есть кнопка "продолжить"

--- bot.py
+++ bot.py
@@ -1,29 +1,51 @@
 import telebot
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup
+from gpt import GPT
 from config import token
-from gpt import ask_gpt
 
 
 bot = telebot.TeleBot(token)
+users = {}
+
+
+def add_user(chat_id):
+    if chat_id not in users.keys():
+        users[chat_id] = GPT()
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    add_user(message.chat.id)
     bot.send_message(message.chat.id, 'hi')
 
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
+    add_user(message.chat.id)
     bot.send_message(message.chat.id, 'helping')
 
 
 def get_prompt(prompt):
-    bot.send_message(prompt.chat.id, ask_gpt(prompt.text))
+    if not prompt.text.startswith('/'):
+        msg = bot.send_message(prompt.chat.id, 'wait')
+        markup = ReplyKeyboardMarkup().add(KeyboardButton('Продолжи'))
+        answer = users[prompt.chat.id].ask_gpt(prompt.text)
+        bot.delete_message(prompt.chat.id, msg.id)
+        bot.send_message(prompt.chat.id, answer, reply_markup=markup)
 
 
 @bot.message_handler(commands=['ask'])
 def ask_message(message):
+    add_user(message.chat.id)
+    users[message.chat.id].assistant = ''
     msg = bot.send_message(message.chat.id, 'print answer')
     bot.register_next_step_handler(msg, get_prompt)
 
 
-bot.polling()
+@bot.message_handler(content_types=['text'])
+def text_message(message):
+    if message.text == 'Продолжи':
+        get_prompt(message)
+
+
+bot.infinity_polling()
--- gpt.py
+++ gpt.py
@@ -1,23 +1,37 @@
 import requests
 
 
-def ask_gpt(question):
-    resp = requests.post(
-        'http://localhost:1234/v1/chat/completions',
-        headers={"Content-Type": "application/json"},
-        json={
-            "messages": [
-                {"role": "system", "content": "Кратко ответь на вопрос на русском"},
-                {"role": "user", "content": question}
-            ],
-            "temperature": 1,
-            'max_tokens': 2000
-        }
-    )
-    try:
-        if resp.status_code == 200 and 'choices' in resp.json():
-            return resp.json()['choices'][0]['message']['content']
-        else:
+class GPT:
+    def __init__(self):
+        self.url = 'http://localhost:1234/v1/chat/completions'
+        self.headers = {'Content-Type': 'application/json'}
+        self.json = {
+                'messages': [
+                    {'role': 'system', 'content': 'По-русски кратко ответь на вопрос: '},
+                ],
+                'temperature': 1.2,
+                'max_tokens': 2000
+            }
+        self.assistant = ''
+
+    def create_json(self, question):
+        self.json['messages'].append({'role': 'user', 'content': question})
+        if self.assistant:
+            self.json['messages'].append({'role': 'assistant', 'content': self.assistant})
+
+    def ask_gpt(self, question):
+        self.create_json(question)
+        try:
+            resp = requests.post(self.url, headers=self.headers, json=self.json)
+            if resp.status_code == 200 and 'choices' in resp.json():
+                self.assistant = self.assistant + resp.json()['choices'][0]['message']['content']
+                del self.json['messages'][1]
+                try:
+                    del self.json['messages'][1]
+                except IndexError:
+                    pass
+                return resp.json()['choices'][0]['message']['content']
+            else:
+                return 'error'
+        except requests.exceptions.ConnectionError:
             return 'error'
-    except:
-        return 'error'
